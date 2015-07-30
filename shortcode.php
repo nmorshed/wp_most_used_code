9ab547011d7f101407f6e607694626b51fa68613
devscorner <mniajx@gmail.com>
2015-07-31 00:46:14 +0600

    Shortcode button for twitter

---
+++ shortcode.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This code is for example of a shortcode which add a twitter
+ * button by shortcode
+ *
+ * function reference:
+ * 1. <?php add_shortcode( $tag , $func ); ?>
+ * 2. <?php shortcode_atts( $pairs , $atts, $shortcode ); ?>
+ */
+add_shortcode('twitter', function( $atts, $text ){
+	// $atts = the attribute inside shortcode -> an array
+	// The text inside shortcode tag -> a variable
+	
+	$atts = shortcode_atts(
+		array(
+			'username' => 'niaj_morshed',
+			'text' => empty($text) ? 'Follow me on twitter' : $text
+		), $atts, 'twitter'
+		// $atts = User defined attributes in shortcode tag
+		// 'twitter' = shortcode name
+	);
+	
+	return "<a href='http://twitter.com/" .$atts['username']. "'>". $atts['text'] . "</a>";
+
+});
+
+?>
